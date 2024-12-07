47e68eab2d4d62b493b99ab79471f49aa3a4eb1c
ayeshasuchi <ayesha.suchi123@gmail.com>
2024-12-07 22:23:29 +0600

    Define module data

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,6 +2,7 @@
     'name':'Real Estate',
     'depends':['base'],
     'data':[
+        'data/estate.property.type.csv',
         'security/ir.model.access.csv',
         'views/estate_property_offer_views.xml',
         'views/estate_property_views.xml',
@@ -9,6 +10,10 @@
         'views/estate_property_tag_views.xml',
         'views/res_users_view.xml',
         'views/estate_menus.xml',
+    ],
+    'demo':[
+        'demo/estate_property.xml',
+        'demo/estate_property_offer_demo.xml',
     ],
     'application': True
 }
